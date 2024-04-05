f3857b39a056339beff8d9c81b425af062b3f569
Harshad <harshad.pawar87@gmail.com>
2024-04-05 20:52:40 +0530

    removed unused file

--- static/js/add_recipe.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addIngredientButton = document.getElementById("addIngredient");
-    const ingredientsContainer = document.getElementById("ingredients");
-
-    addIngredientButton.addEventListener("click", function() {
-        const ingredientTemplate = document.querySelector(".ingredient").cloneNode(true);
-        ingredientsContainer.appendChild(ingredientTemplate);
-    });
-});
